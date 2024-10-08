3dcad2305e8c95cd164a56971704702cb80bf643
ibenmain <issambenmaina92@gmail.com>
2024-10-08 17:18:18 +0100

    feat: add register app

--- backend/urls.py
+++ backend/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("register/", include("register.urls")),
     path("admin/", admin.site.urls),
 ]
